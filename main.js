2a7314a041410185323ab7f7f44155973b03893f
Domninic <liamdomnic@gmail.com>
2020-09-14 16:55:34 +0300

    first-commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// var img=document.getElementById("slider");
+
+// var images=["images/a.png", "images/b.png", "images/c.png", "images/d.png","images/e.png"];
+// var x=0;
+
+// function slide(){
+// 	if (x<images.length) {
+// 		x=x+1;
+// 	}else{
+// 		x=1;
+// 	}
+
+// 	img.innerHTML="<img src="+images[x-1]+">";
+
+	
+// }
+// img.style.height="50%";
+// setInterval(slide,2000);
+
+var date=new Date();
+document.getElementById("year").innerHTML=date.getFullYear();
+
